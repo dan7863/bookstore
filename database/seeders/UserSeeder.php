18d4b2bb11d2d6266ddfe5c9786dfa333ccdc72d
dan7863 <danrico786@gmail.com>
2023-12-02 11:01:33 -0500

    feat(gitignore): Ignore scannerwork

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,6 +18,6 @@ class UserSeeder extends Seeder
             'email' => 'dan@gmail.com',
             'password' => bcrypt('12345678')
         ]);
-        User::factory(99)->create();
+        User::factory(4)->create();
     }
 }
